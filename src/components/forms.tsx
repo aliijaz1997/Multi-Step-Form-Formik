53ad3431ab9088a52105758fd92fae4d42f7caed
aliijaz1997 <57563582+aliijaz1997@users.noreply.github.com>
2020-10-14 00:41:01 +0500

    Done with necassary installation

---
+++ src/components/forms.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import {Formik} from 'formik';
+
+
+export function Form1 () {
+  return (
+    <div>Hello from Form 1</div>
+  )
+}
+export function Form2 () {
+  return (
+    <div>Hello from Form 2</div>
+  )
+}
+export function Form3 () {
+  return (
+    <div>Hello from Form 3</div>
+  )
+}
