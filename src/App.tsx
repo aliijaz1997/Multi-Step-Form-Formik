0e202b9b090b402f3137084ec875b41d63735e66
aliijaz1997 <57563582+aliijaz1997@users.noreply.github.com>
2020-10-15 00:28:02 +0500

    Done with Form 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import {Form1,Form2,Form3} from './components/forms';
-function App() {
-  return (
-    <div className="App">
-     hello from main
-    </div>
-  );
-}
+import { Step, StepLabel, Stepper } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Form2, Form1, Form3 } from './components/forms'
+import "./components/forms.css";
 
-export default App;
+  const steplabel = () => {
+    return ['1 out of 3', '2 out of 3', '3 out of 3']
+  }
+export default function App() {
+  const [currentStep, setCurrentStep] = useState(0)
+  // function to get the content of all forms
+  const getcontentofstep = (stepIndex: number, gettonextstep: () => void) => {
+    switch (stepIndex) {
+      case 0:
+        return <Form1 handlenext={gettonextstep} />
+      case 1:
+        return <Form2 />
+      case 3:
+        return <Form3 />
+        default :
+        return "Unknown step index";
+    }
+  }
+  // function to go on next step
+
+  const gettonextstep = () => {
+    setCurrentStep((step : number) => step + 1)
+    console.log("clicking next");
+    
+  }
+  return (
+    <div className = "background">
+      <h1 className="heading">Multi-Step Form</h1>
+      <Stepper activeStep = {currentStep} style = {{backgroundColor : "Linen"}} alternativeLabel >
+      {steplabel().map((value) => (
+          // return (
+            <Step key={value}>
+              <StepLabel>{value}</StepLabel>
+            </Step>
+          // )
+      ))}
+      </Stepper>
+      <div>
+        {getcontentofstep(currentStep, gettonextstep)}
+      </div>
+
+    </div>
+  )
+}
--- src/components/forms.tsx
+++ src/components/forms.tsx
@@ -1,19 +1,100 @@
 import React from 'react';
-// import {Formik} from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
+import * as yup from 'yup';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/material.css';
+import "./forms.css"
+interface props {
+  handlenext: () => void;
+}
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '350px',
+      },
+    },
+  }),
+);
 
+export const Form1: React.FC<props> = ({ handlenext }) => {
+  const classes = useStyles();
 
-export function Form1 () {
   return (
-    <div>Hello from Form 1</div>
+    <div className="div" >
+      <Formik
+        initialValues={{ firstname: '', secondname: '', email: '', cnic: 0 }}
+        validationSchema={yup.object({
+          firstname: yup.string().max(15, 'Name should not be more than 15 character').required('Required'),
+          secondname: yup.string().max(15, 'Name should not be more than 15 character').required('Required'),
+          email: yup.string().email().required('The field is mandatory'),
+          cnic: yup.number().integer().min(13,'Type 13 digit').required('Required').positive('Type positive number')
+        })}
+        onSubmit={(values) => {
+          console.log(values)
+          handlenext();
+        }}
+      >
+        <Form className={classes.root}  autoComplete="off">
+          <Field id="standard-basic" label="First Name" name='firstname' type='text' />
+          <br/>
+          <span className = "Error">
+          <ErrorMessage name="firstname" />
+          </span>
+          <br/>
+          <Field id="standard-basic" label="Second Name" name='secondname' type='text' />
+          <br />
+          <span className = "Error">
+          <ErrorMessage name="secondname" />
+          </span>
+          <br />
+          <Field id="standard-basic" label="Email" name='email' type='text' />
+          <br/>
+          <span className = "Error">
+          <ErrorMessage name="email" />
+          </span>
+          <br />
+          <Field id="standard-basic" label="CNIC" name='cnic' type='text' />
+          <br/>
+          <span className = "Error">
+          <ErrorMessage name="cnic" />
+          </span>
+          <br />
+          <PhoneInput
+            country={'pk'}
+            inputProps={{
+              // country:{'us'},
+              name: 'phone',
+              required: true,
+              autoFocus: true,
+            }}
+          />
+          <Button variant="contained" type="submit" color="primary" disableElevation>
+           Next Section
+           </Button>
+        </Form>
+        {/* {/* </div> */}
+      </Formik>
+    </div>
   )
 }
-export function Form2 () {
+
+export const Form2 = () => {
   return (
-    <div>Hello from Form 2</div>
+    <div>
+      Hello from Form 2
+    </div>
   )
 }
-export function Form3 () {
+
+export const Form3 = () => {
   return (
-    <div>Hello from Form 3</div>
+    <div>
+      Hello from Form 3
+    </div>
   )
 }
